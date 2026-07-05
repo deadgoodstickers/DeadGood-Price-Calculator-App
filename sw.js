3359a69304dd62d8d0d924a2812dd2388122dd2e
deadgoodstickers <deadgoodstickers@gmail.com>
2026-07-05 08:12:40 +0100

    Garment cat update

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "deadgood-quote-engine-v3-30";
+const CACHE_NAME = "deadgood-quote-engine-v3-32";
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./assets/styles.css",
-  "./assets/styles.css?v=rc25",
+  "./assets/styles.css?v=rc27",
   "./assets/app.js",
   "./assets/app.js?v=rc17",
   "./assets/config.js",
