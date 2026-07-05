697ebe05faccfb1c30da51099cba43484d739f40
deadgoodstickers <deadgoodstickers@gmail.com>
2026-07-05 07:36:06 +0100

    desktop fix

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "deadgood-quote-engine-v3-24";
+const CACHE_NAME = "deadgood-quote-engine-v3-26";
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./assets/styles.css",
-  "./assets/styles.css?v=rc19",
+  "./assets/styles.css?v=rc21",
   "./assets/app.js",
   "./assets/app.js?v=rc17",
   "./assets/config.js",
