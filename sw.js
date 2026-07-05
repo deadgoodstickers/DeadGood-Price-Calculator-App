87c957d74639004588a16ae741ebbd34a4f1355d
deadgoodstickers <deadgoodstickers@gmail.com>
2026-07-05 08:26:09 +0100

    darkened cards again

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "deadgood-quote-engine-v3-34";
+const CACHE_NAME = "deadgood-quote-engine-v3-35";
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./assets/styles.css",
-  "./assets/styles.css?v=rc29",
+  "./assets/styles.css?v=rc30",
   "./assets/app.js",
   "./assets/app.js?v=rc17",
   "./assets/config.js",
